1bb7c501bb05daeeb89702050cc9566c5edaf8b0
Caylak, Cem <Cem.Caylak@adesso.de>
2022-11-15 09:37:34 +0100

    first commit

---
+++ src/main/java/de/caylak/babackend/BaBackendApplication.java
@@ -0,0 +1,13 @@
+package de.caylak.babackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/caylak/babackend/BaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package de.caylak.babackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
